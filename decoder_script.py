8908cae2f04a5da04a49ffdfb1b4a2dbfef5fecd
Nicholas Johnson <nejohnson2@gmail.com>
2015-11-04 22:34:36 -0500

    updated script to add properties to geojson - major revision

--- decoder_script.py
+++ decoder_script.py
@@ -29,28 +29,69 @@ def load_data(fileName):
     data = pd.read_csv(fileName, error_bad_lines=False)
     return data        
 
-def create_feature(record):
+def create_feature(data):
+    '''extract individual landfill features
+    with their associated properties'''
+
+    record = data[0]
     if record != '0':
         record = ast.literal_eval(record)
+        
+        # loop through records if there are multiple
         for i in record:
             geometry = ast.literal_eval(i['area_geometry'])
-            properties = i['area']
-            obj_id = i['id']
             result = {
                 "type": "Feature",
                 "geometry": geometry,
-                "properties": {"area" : properties, "id" : obj_id}
+                "properties": {
+                    "area" : i['area'], 
+                    "id" : i['id'], 
+                    "fac_name" : data[1],
+                    "fac_state" : data[2],
+                    "fac_street" : data[3],
+                    "task_id" : data[4],
+                    "task_user_id" : data[5],
+                    "task_runinfo__skip" : data[6],
+                }
             }
-
-            return result
+    else:
+        result = {
+            "type": "Feature",
+            "geometry": None,
+            "properties": {
+                "area" : 0,
+                "id" : None,
+                "fac_name" : data[1],
+                "fac_state" : data[2],
+                "fac_street" : data[3],
+                "task_id" : data[4],
+                "task_user_id" : data[5],
+                "task_runinfo__skip" : data[6],
+            }
+        }
+    
+    return result            
 
 def create_geojson(data):
-	print "Creating geojson..."
-	features = list(filter(None, map(create_feature, data)))
-	return json.dumps({
-		"type": "FeatureCollection",
-		"features": features,
-	})
+    '''collect features to make geojson'''
+    # this is a hack to map data columns to the 
+    # create_feature function.
+    temp = zip(
+                list(data['task_runinfo__area']),
+                list(data['task_runinfo__Facility_Name']),
+                list(data['task_runinfo__Facility_State']),
+                list(data['task_runinfo__Facility_Street']),
+                list(data['task_run__task_id']),
+                list(data['task_run__user_id']),
+                list(data['task_runinfo__skip']),    
+              )
+
+    features = list(map(create_feature, temp))
+    
+    return json.dumps({
+        "type": "FeatureCollection",
+        "features": features,
+    })    
 
 def writeFile(geojson):
 	print "Writing geojson file..."
@@ -60,6 +101,6 @@ def writeFile(geojson):
 
 if __name__ == '__main__':
 	data = retrieveData()
-	geojson = create_geojson(data['task_runinfo__area'])
+	geojson = create_geojson(data)
 	writeFile(geojson)
 	print "Finished"
